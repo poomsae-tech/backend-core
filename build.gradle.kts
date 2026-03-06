c9705192ff3b1af5acdb57b135adccba0dcd5d51
Vitaly Vlasov <v.vlasov@MacBook-Pro-Vitalij.local>
2026-03-06 16:21:51 +0700

    feat-15 add sso adapter

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,15 @@ repositories { mavenCentral() }
 dependencies {
   // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.security:spring-security-oauth2-resource-server")
+  implementation("org.springframework.security:spring-security-oauth2-jose")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("tools.jackson.module:jackson-module-kotlin")
   // runtimeOnly("org.postgresql:postgresql")
   testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+  testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/main/kotlin/ru/poomsae/core/driver/http/MessageController.kt
+++ src/main/kotlin/ru/poomsae/core/driver/http/MessageController.kt
@@ -1,10 +1,25 @@
-package ru.poomsae.core
+package ru.poomsae.core.driver.http
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import ru.poomsae.core.security.AuthenticatedUser
 
 @RestController
 class MessageController {
-  @GetMapping("/") fun index(@RequestParam("name") name: String) = "Hello, $name!"
+
+    @GetMapping("/api/me")
+    fun me(@AuthenticationPrincipal jwt: Jwt): Map<String, Any?> {
+        val user = AuthenticatedUser(jwt)
+        return mapOf(
+            "userId" to user.userId,
+            "username" to user.username,
+            "email" to user.email,
+            "roles" to user.roles,
+            "orgId" to user.orgId,
+            "judgeType" to user.judgeType,
+        )
+    }
 }
+
